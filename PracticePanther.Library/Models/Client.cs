24b8fa8adfd9175271ba822bd15bc9d8c5a87fbd
mghar <mghar@MASONSLAPTOP>
2023-05-27 22:14:40 -0400

    Cleaned up project.cs

--- PracticePanther.Library/Models/Client.cs
+++ PracticePanther.Library/Models/Client.cs
@@ -19,123 +19,13 @@ namespace PracticePanther.CLI.Models
         public Client()
         {
             Name = string.Empty;
-            Notes= string.Empty;
+            Notes = string.Empty;
         }
 
         public override string ToString()
         {
             return $"{Id}. {Name}  (Open Date: {OpenDate} Close Date: {CloseDate}) Notes: {Notes}";
         }
-
-
-
-
-
-
-
-
-
-
-        /*
-        
-        
-        
-        
-        private int id;
-        private DateTime opendate, closedate;
-        private bool isactive;
-        private string name, notes;
-        public int Id
-        {
-            get
-            {
-                return id;
-            }
-            set
-            {
-          
-            }
-        }
-        public DateTime OpenDate
-        {
-            get
-            {
-                return opendate;
-            }
-            set
-            {
-
-            }
-        }
-
-        public DateTime CloseDate
-        {
-            get
-            {
-                return closedate;
-            }
-            set
-            {
-
-            }
-        }
-
-        public bool IsActive
-        {
-            get
-            {
-                return isactive;
-            }
-            set
-            {
-
-            }
-        }
-
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-
-            }
-        }
-
-        public string Notes
-        {
-            get
-            {
-                return notes;
-            }
-            set
-            {
-
-            }
-        }
-
-        public List<Project> ClientProjects
-        {
-            get
-            {
-                return ClientProjects;
-            }
-            set
-            {
-
-            }
-        }
-     
-
-
-
-
-
-
-
-
-        */
+       
     } 
 }
--- PracticePanther.Library/Models/Project.cs
+++ PracticePanther.Library/Models/Project.cs
@@ -9,118 +9,29 @@ namespace PracticePanther.CLI.Models
     public class Project
     {
 
-
-
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime OpenDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public bool IsActive { get; set; }
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+        public int ClientId { get; set; } 
+
 
 
 
         public Project()
         {
             Name = string.Empty;
+            ShortName = string.Empty;
+            LongName = string.Empty;
         }
 
         public override string ToString()
         {
             return $"{Id}. {Name}";
         }
-
-
-        /*
-        private int Id, ClientId;
-        private DateTime OpenDate, CloseDate;
-        private bool IsActive;
-        private string ShortName, LongName;
-
-        public int id
-        {
-            get
-            {
-                return Id;
-            }
-            set
-            {
-
-            }
-        }
-
-        public int clientid
-        {
-            get
-            {
-                return ClientId;
-            }
-            set
-            {
-
-            }
-        }
-
-        public DateTime opendate
-        {
-            get
-            {
-                return OpenDate;
-            }
-            set
-            {
-
-            }
-        }
-
-        public DateTime closedate
-        {
-            get
-            {
-                return CloseDate;
-            }
-            set
-            {
-
-            }
-        }
-
-        public bool isactive
-        {
-            get
-            {
-                return IsActive;
-            }
-            set
-            {
-
-            }
-        }
-
-        public string shortname
-        {
-            get
-            {
-                return ShortName;
-            }
-            set
-            {
-
-            }
-        }
-
-        public string longname
-        {
-            get
-            {
-                return LongName;
-            }
-            set
-            {
-
-            }
-        }
-
-
-
-        */
-
 
     }
 }
