8c6fd87ca7e087ffdd5d3be87d76a24cbaaf3d59
mhartle02 <mhartle02@gmail.com>
2023-06-15 10:44:52 -0400

    UI Rework

--- PracticePanther.MAUI/MainPage.xaml.cs
+++ PracticePanther.MAUI/MainPage.xaml.cs
@@ -4,33 +4,15 @@ namespace PracticePanther.MAUI
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = new MainViewModel();
-
         }
 
-        private void SearchClicked(object sender, EventArgs e)
+        private void ClientsClicked(object sender, EventArgs e)
         {
-            (BindingContext as MainViewModel).Search();
-       
+            Shell.Current.GoToAsync("//Clients");
         }
-
-        private void DeleteClick(object sender, EventArgs e)
-        {
-            (BindingContext as MainViewModel).Delete();
-        }
-
-        private void ClientClicked(object sender, EventArgs e)
-        {
-            Shell.Current.GoToAsync("//Client");
-        }
-
-        private void NewClientClicked(object sender, EventArgs e)
-        {
-            Shell.Current.GoToAsync("NewClient");
-        }
-
     }
 }
