c6aa6d96a102a4e193d7463f7a6f538e78ca4eec
mhartle02 <mhartle02@gmail.com>
2023-07-03 15:08:15 -0400

    Server side

---
+++ PracticePanther.API/PracticePanther.API/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticePanther.API.Database;
+using PracticePanther.Library.Models;
+
+namespace PracticePanther.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController: ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Client> Get()
+        {
+            return FakeDatabase.Clients;
+        }
+    }
+}
---
+++ PracticePanther.API/PracticePanther.API/Database/FakeDatabase.cs
@@ -0,0 +1,17 @@
+﻿using PracticePanther.Library.Models;
+
+namespace PracticePanther.API.Database
+{
+    public static class FakeDatabase
+    {
+        public static List<Client> Clients = new List<Client>
+        {
+            new Client{Id = 1, Name = "Client 1"},
+            new Client{Id = 2, Name = "Client 2"},
+            new Client{Id = 3, Name = "Client 3"},
+            new Client{Id = 4, Name = "Client 4"},
+            new Client{Id = 5, Name = "Client 5"},
+            new Client{Id = 6, Name = "Client 6"},
+        };
+    }
+}
